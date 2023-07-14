1ca7a5100d79a5c43244533b2b2e8188f3959d18
Maciej Skórski <maciej.skorski@gmail.com>
2023-07-14 20:32:56 +0000

    clean code

---
+++ src/main/java/autotester/App.java
@@ -0,0 +1,14 @@
+package autotester;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import autotester.AnimalClass;
+import autotester.PersonClass;
+
+class App {
+
+   public static void main(String[] args) {
+   }
+}
